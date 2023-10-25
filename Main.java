9a6d60f79b1e3fb813edf96012518d7ea3939415
Atharv Thakur <46360592+NonabstractPhoton@users.noreply.github.com>
2023-10-25 13:30:10 -0400

    Part 1

--- Main.java
+++ Main.java
@@ -1,8 +1,38 @@
-public class Main{
-    public static void main(String[] args){
-        System.out.println("hello world!");
-        System.out.println("hello world!");
+class Main
+{
+    public static void main(String[] args) {
+        int[][] a = squareArrayN(5);
+        print2DArr(a);
+    }
 
-        System.out.println("this was done on the browser");
+    static int[][] squareArrayN(int n)
+    {
+        if (n < 0)
+            throw new IndexOutOfBoundsException(); 
+
+        int[][] arr = new int[n][n];
+
+        for (int[] row : arr) 
+            randomlyFillArray(row);
+        
+        return arr;
+    }
+
+    static void randomlyFillArray(int[] arr)
+    {
+        for (int i = 0; i < arr.length; i++)
+        {
+            arr[i] = (int)(Math.random()*10+1);
+        }
+    }
+
+    static void print2DArr(int[][] arr)
+    {
+        for (int i = 0; i < arr.length; i++)
+        {
+            for (int j = 0; j < arr[i].length;j++)
+                System.out.print(arr[i][j]+"\t");
+            System.out.println();
+        }
     }
 }
