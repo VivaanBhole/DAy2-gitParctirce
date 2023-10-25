7c954257a7eb213439bdf92f0361d6ab3c1acebe
Atharv Thakur <46360592+NonabstractPhoton@users.noreply.github.com>
2023-10-25 13:34:19 -0400

    Part 2: 1

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ class Main
     public static void main(String[] args) {
         int[][] a = squareArrayN(5);
         print2DArr(a);
+        System.out.println("First Even: "+analyze2DArray(a));
     }
 
     static int[][] squareArrayN(int n)
@@ -16,6 +17,26 @@ class Main
             randomlyFillArray(row);
         
         return arr;
+    }
+
+    static int analyze2DArray(int[][] arr)
+    {
+        for (int[] row : arr)
+        {
+            int n = firstEven(row);
+            if (n != -1)
+                return n;
+        }
+
+        return -1;
+    }
+
+    static int firstEven(int[] arr)
+    {
+        for (int i : arr)
+                if (i % 2 == 0)
+                    return i;
+        return -1;
     }
 
     static void randomlyFillArray(int[] arr)
